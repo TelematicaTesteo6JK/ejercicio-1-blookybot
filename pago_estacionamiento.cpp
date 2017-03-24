bae5a0aa859ea8d13c20335eb97577dd789e02eb
Avalos Galindo Marcos Antonio <mavalos8@ucol.mx>
2017-03-24 10:21:19 -0600

    rama 1 del parking

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -24,6 +24,15 @@ void casos_de_prueba(){
 
     cout << "TEST CASES" << endl;
     // Agrega tus casos de prueba
+cout<<"Test data: 1/ expected result:10/ actual result:"<<pago_estacionamiento(1)<<endl;
+cout<<"Test data: 5/ expected result:50/ actual result:"<<pago_estacionamiento(5)<<endl;
+cout<<"Test data: 6/ expected result:54/ actual result:"<<pago_estacionamiento(6)<<endl;
+cout<<"Test data: 10/ expected result:98/ actual result:"<<pago_estacionamiento(10)<<endl;
+cout<<"Test data: 11/ expected result:77/ actual result:"<<pago_estacionamiento(11)<<endl;
+cout<<"Test data: 0/ expected result:0/ actual result:"<<pago_estacionamiento(0)<<endl;
+cout<<"Test data: 4/ expected result:40/ actual result:"<<pago_estacionamiento(4)<<endl;
+cout<<"Test data: 8/ expected result:72/ actual result:"<<pago_estacionamiento(8)<<endl;
+cout<<"Test data: 13/ expected result:91/ actual result:"<<pago_estacionamiento(13)<<endl;
 
 
 
